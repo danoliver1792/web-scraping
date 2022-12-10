73232a926ff9560e8aa51ea665f574ce380d16eb
dan <danrleioliveira1792@gmail.com>
2022-12-09 23:12:56 -0300

    script para coletar temperatura em curitiba

---
+++ clima_cidade/scraping.py
@@ -0,0 +1,18 @@
+import requests as req
+from bs4 import BeautifulSoup as bs4
+
+# buscando o html do site
+html = req.get('https://www.climatempo.com.br/previsao-do-tempo/cidade/271/curitiba-pr').content
+
+soup = bs4(html, 'html.parser')
+
+# coletando a temperatura minima
+print('Temperatura em Curitiba:')
+tempMin = soup.find(id='min-temp-1')
+print(f'minima de {tempMin.text}')
+
+tempMax = soup.find(id='max-temp-1')
+print(f'maxima de {tempMax.text}')
+
+textTemp = soup.find(class_='-gray -line-height-24 _center')
+print(textTemp.text)
